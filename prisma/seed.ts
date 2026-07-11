2a4df0dcdd5e4dff7fc186b2442e44fa3611749b
Newton <156936866+Newton2n@users.noreply.github.com>
2026-07-11 18:39:09 +0600

    feat(seed): add Prisma seed script with admin and default categories

---
+++ prisma/seed.ts
@@ -0,0 +1,98 @@
+import bcrypt from "bcryptjs";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import config from "../src/config";
+import {
+  PrismaClient,
+  UserActiveStatus,
+  UserRole,
+} from "../generated/prisma/client";
+
+const pool = new Pool({
+  connectionString: config.database_url,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+async function main() {
+  console.log(" Seeding database...");
+
+  // Hash  password
+  const hashedPassword = await bcrypt.hash(
+    "123456",
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  // Admin
+  await prisma.user.upsert({
+    where: {
+      email: "admin@gmail.com",
+    },
+    update: {},
+    create: {
+      name: "Admin",
+      email: "admin@gmail.com",
+      password: hashedPassword,
+      role: UserRole.ADMIN,
+      status: UserActiveStatus.ACTIVE,
+    },
+  });
+
+  // Categories
+  const categories = [
+    {
+      name: "Electrical",
+      description: "Electrical installation, maintenance, and repair services.",
+    },
+    {
+      name: "Plumbing",
+      description: "Plumbing installation, maintenance, and repair services.",
+    },
+    {
+      name: "Cleaning",
+      description: "Residential and commercial cleaning services.",
+    },
+    {
+      name: "Painting",
+      description: "Interior and exterior painting services.",
+    },
+    {
+      name: "Carpentry",
+      description: "Woodworking and furniture repair services.",
+    },
+    {
+      name: "AC Repair",
+      description: "Air conditioner installation and repair services.",
+    },
+    {
+      name: "Home Appliance Repair",
+      description: "Repair services for home appliances.",
+    },
+  ];
+
+  for (const category of categories) {
+    await prisma.category.upsert({
+      where: {
+        name: category.name,
+      },
+      update: {},
+      create: category,
+    });
+  }
+
+  console.log("Database seeded successfully.");
+}
+
+main()
+  .catch((error) => {
+    console.error("Seed failed:", error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+    await pool.end();
+  });
